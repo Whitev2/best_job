72cf5f3a695541677be25e8e114e0dbcd00ca55c
What do you do? <109747879+Whitev2@users.noreply.github.com>
2022-11-08 17:02:56 +0500

    version 1.0.8

--- handlers/users/menu.py
+++ handlers/users/menu.py
@@ -1,4 +1,5 @@
 import asyncio
+from datetime import datetime
 
 from aiogram import Router, F
 from aiogram import types
@@ -6,6 +7,7 @@ from aiogram.dispatcher.fsm.context import FSMContext
 from aiogram.types import Message
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
+from DataBase.base import sql_get_last_rows
 from filters.driver_filter import IsDriver
 from states.driver_register import driver_reg
 
@@ -26,7 +28,15 @@ async def user_cabinet(message: Message, state: FSMContext):
 
 @router.message(F.text == 'История заказов')
 async def orders_history(message: Message, state: FSMContext):
-    await message.answer("Все ваши заказы за две недели:")
+    last_orders = await sql_get_last_rows(user_id=str(message.from_user.id), limit=10)
+    print(last_orders)
+    text = f"Последние {len(last_orders)} заказов:\n\n"
+    for order in last_orders:
+        datestr = order[1]
+        cuont_stop = len(order[2])
+        order_date = datetime.strptime(datestr, "%Y-%m-%d %H:%M:%S.%f")
+        text = text + f"Дата заказа: {str(order_date)[:-10]}\nКоличество остановок: {cuont_stop}\n_______________\n"
+    await message.answer(text)
 @router.message(F.text == 'Мой заработок')
 async def user_balance(message: Message, state: FSMContext):
     await message.answer("Ваш заработок за две недели:")
