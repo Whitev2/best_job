33fc2acebd565ed4704d58818284ca1f7b216bcc
What do you do? <109747879+Whitev2@users.noreply.github.com>
2022-11-13 22:55:43 +0500

    version 1.2.5

--- payments/payroll.py
+++ payments/payroll.py
@@ -1,10 +1,25 @@
-async def salary(count):
-    x_one = 1500
-    x_other = 750
+from DataBase.base import User
+
+user = User()
+
+
+async def salary(telegram_id: str, count):
+    car_mass = await user.get_car_mass(telegram_id)
+    sum_start = 0
+    x_other = 0
+    if car_mass[0][0] == '2.5':
+        sum_start = 1500
+        x_other = 750
+    elif car_mass[0][0] == '5':
+        sum_start = 1500
+        x_other = 750
+    elif car_mass[0][0] == '10':
+        sum_start = 1500
+        x_other = 750
+
     if count >= 2:
         point = count - 1
         sum_of_point = point * x_other
-        sum_start = x_one
         return float(sum_of_point + sum_start)
     else:
-        return x_one
+        return sum_start
